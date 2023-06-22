f9ddee9c4e7f984436cbc8eda7851ac2539ec780
DarthSanso <zdsparky@gmail.com>
2023-06-22 12:54:02 -0400

    Deploy

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "RoboFriends",
+    subtitle: "React",
+    description:
+      "A small project that showcases the use of search filtering and swift responsiveness.",
+    image: "./projectrf.jpg",
+    link: "https://darthsanso.github.io/robofriends/",
+  },
+  {
+    title: "Background Color Generator",
+    subtitle: "JavaScript",
+    description:
+      "A small project that allows you to change the background colors of a live webpage.",
+    image: "./projectbgg.jpg",
+    link: "https://darthsanso.github.io/background-generator/",
+  },
+  {
+    title: "Random Quote Generator",
+    subtitle: "React",
+    description:
+      "A little project that randomly pulls quotes from a large API at the touch of a button!",
+    image: "./projectqg.jpg",
+    link: "https://darthsanso.github.io/quotegenerator/",
+  },
+  {
+    title: "Tableau",
+    subtitle: "Data Analysis",
+    description:
+      "A short study I did in 2021 about the comparison between minimum wage and increasing costs of goods.",
+    image: "./tab.png",
+    link: "https://public.tableau.com/app/profile/zachary.sanso/viz/MinimumWage_16219756347780/WeDemandaLivableWage",
+  },
+];
+
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML",
+  "Node",
+  "PostgreSQL",
+  "MySQL",
+  "CSS",
+  "Bootstrap",
+  "Tailwind",
+  "Git",
+  "Github",
+  "Tableau"
+];
